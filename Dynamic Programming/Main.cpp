fc0118eb09042d2cc2efa9800a36b06744773d92
DarkDreth <jordoncain@mail.usf.edu>
2016-03-28 22:47:02 -0400

    Reads from input.txt

--- Dynamic Programming/Main.cpp
+++ Dynamic Programming/Main.cpp
@@ -2,14 +2,34 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 int main()
 {
+	string line;
+	ifstream file ("input.txt");
+	int count = 0;
+	vector<vector<int>> arguments;
 
-}
+	if(file.is_open())
+	{
+		getline(file, line);
+		count = atoi(line.c_str());
+		while( getline(file, line))
+		{
+			vector<int> temp;
+			stringstream stream(line);
+			int num;
+			while(stream >> num)
+			{
+				temp.push_back(num);
+			}
 
-void readFile(string file)
-{
+			arguments.push_back(temp);
+		}
+	}
 
 }
